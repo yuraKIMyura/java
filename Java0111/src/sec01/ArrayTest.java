74207dafc325cc5bc24ae521c45cd94b90eb400d
yuraKIMyura <yurakim.ca23@gmail.com>
2024-01-11 16:32:28 +0900

    small changes

--- Java0111/src/sec01/ArrayTest.java
+++ Java0111/src/sec01/ArrayTest.java
@@ -6,20 +6,30 @@ public class ArrayTest {
 	public static void main(String[] args) {
 				
 //[문제1-3] 1.합계, 2.평균, 3.최댓값
+		System.out.println("[문제1-3] 1.합계 2.평균 3.최댓값");
 		int a [] = {1, 3, 10, 2, 8};
 		calculate(a);
 
 //[문제4] 4가 있는 index 출력
+		System.out.println();
+		System.out.println("[문제4] 4가 있는 인덱스 출력");
+		System.out.println("{9, -1, 10, 3, 1, 88, 4, 2, 4, -13}");
 		int b [] = {9, -1, 10, 3, 1, 88, 4, 2, 4, -13};
 		findIndex(4, b);
 
 //[문제5] x와 y 값을 바꾸는 프로그램
+		System.out.println();
+		System.out.println("[문제4] x=10, y=20 바꾸기");
 		int x = 10;
 		int y = 20;
 		exchange(x,y);
 		
-//[문제6] 버블소트	
-		bubbleSort(b);
+//[문제6] 버블정렬
+		System.out.println();
+		System.out.println("[문제5] 버블 정렬 알고리즘");
+		System.out.println("{9, -1, 10, 3, 1, 88, 4, 2, 4, -13}");
+		bubbleSortAsc(b);
+		bubbleSortDesc(b);
 		
 	}//main
 
@@ -41,9 +51,11 @@ public class ArrayTest {
 	}//method
 	
 	public static void findIndex(int number, int[] array) {
+		int count = 1;
 		for(int i=0; i<array.length; i++) {
 			if(array[i]==number) {
-				System.out.println(number+"의 index: "+i);
+				System.out.println(number+"의 "+count+"번째 인덱스: "+i);
+				count++;
 			}
 		}
 	}//method
@@ -56,7 +68,8 @@ public class ArrayTest {
 		System.out.println("x: "+num1+", y: "+num2);
 	}//method
 	
-	public static void bubbleSort(int[] array) {
+	public static void bubbleSortAsc(int[] array) {
+		System.out.print("오름차순 ");
 		
 		for(int i=0; i<array.length; i++) {
 			for(int j=0; j<array.length-1; j++) {
@@ -72,11 +85,28 @@ public class ArrayTest {
 		
 	}//method
 	
+	public static void bubbleSortDesc(int[] array) {
+		System.out.print("내림차순 ");
+		
+		for(int i=0; i<array.length; i++) {
+			for(int j=0; j<array.length-1; j++) {
+				if(array[j]<array[j+1]) {
+					int temp = array[j];
+					array[j] = array[j+1];
+					array[j+1] = temp;
+				}
+			}
+		}//for
+		
+		printArray(array);
+		
+	}//method
+	
 	
 	public static void printArray(int[] array) {
 		for(int i=0; i<array.length; i++) {
 				System.out.print(array[i]+" ");
-		}
+		}System.out.println();
 	}//method
 
 }//class
