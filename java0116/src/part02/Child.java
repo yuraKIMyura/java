5027cd31f035b5909954aaa5b0864689529a7239
yuraKIMyura <yurakim.ca23@gmail.com>
2024-01-16 14:30:31 +0900

    superclass constructor

---
+++ java0116/src/part02/Child.java
@@ -0,0 +1,13 @@
+package part02;
+
+public class Child extends Parent{
+	
+	private int studentNo;
+	
+	public Child(String name, int studentNo) {
+		super(name);
+		this.name = name; //redundant하므로 없애는 게 낫다
+		this.studentNo = studentNo;
+	}
+
+}
---
+++ java0116/src/part02/Parent.java
@@ -0,0 +1,11 @@
+//1 superclass constructor
+package part02;
+
+public class Parent {
+	public String name;
+	
+	public Parent(String name) {
+		this.name = name;
+	}
+
+}
