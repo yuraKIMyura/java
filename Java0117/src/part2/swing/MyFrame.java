b0b563fcd90c2c7ae60c7a37f882845a7a467e58
yuraKIMyura <yurakim.ca23@gmail.com>
2024-01-17 14:59:20 +0900

    swing

---
+++ Java0117/src/part2/swing/MyFrame.java
@@ -0,0 +1,24 @@
+//2 java GUI swing
+package part2.swing;
+
+import javax.swing.JFrame;
+
+//1.extends JFrame 해야 함
+public class MyFrame extends JFrame{
+	
+	public MyFrame() {
+//3.MyFrame instance가 보이도록 constructor에서 설정	
+		this.setVisible(true);
+//4.크기 설정
+		this.setSize(300, 300);
+//5.화면을 닫으면 프로그램이 자동 종료되도록 설정 
+//(이 설정 없으면 console에서 직접 terminate 해야 memory에서도 unload됨)
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public static void main(String...args) {
+//2.MyFrame class instantiation	
+		new MyFrame();
+	}
+	
+}
