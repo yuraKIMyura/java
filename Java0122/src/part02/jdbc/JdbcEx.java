e3a982b539bedd158d602199ea64f4629a8fae44
yuraKIMyura <yurakim.ca23@gmail.com>
2024-01-22 17:31:20 +0900

    JDBC

--- Java0122/src/part02/jdbc/JdbcEx.java
+++ Java0122/src/part02/jdbc/JdbcEx.java
@@ -4,7 +4,9 @@ package part02.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JdbcEx {
 	
@@ -23,8 +25,27 @@ public class JdbcEx {
 		//DriverManager is a class in the java.sql package that manages a list of database drivers. It is used to establish a connection to a database by selecting an appropriate driver from the registered JDBC drivers.
 		//getConnection() method is a static method of the DriverManager class. It is used to establish a connection to a database.
 		Connection conn = DriverManager.getConnection(url, id, pass);
-		
 		System.out.println("MySQL 연결 성공");
+		
+
+	/**JDBC Database Query Example**/		
+		//Statement is an interface provided by the java.sql package that allows you to execute SQL queries against a database. 
+		Statement stmt = conn.createStatement();
+		//ResultSet represents the result set of a database query.
+		ResultSet rs = stmt.executeQuery("SELECT * FROM dept");
+		//next() method is a part of the ResultSet interface in Java's JDBC (Java Database Connectivity) API. 
+		//It is used to move the cursor to the next row in the result set and returns a boolean value indicating whether there is a next row or not.
+		while(rs.next()) {
+			int deptno = rs.getInt("deptno");
+			String dname = rs.getString("dname");
+			String loc = rs.getString("loc");
+			System.out.printf("%d, %s, %s\n", deptno, dname, loc);
+		}
+		rs.close();
+		
+		
+		
+		
 		conn.close();
 	}
 
