e399627bc829f73968ed7f17f727a73450a208e0
yuraKIMyura <yurakim.ca23@gmail.com>
2024-01-25 19:45:52 +0900

    project

--- Java0125/src/part02/evaluation/CLIservice.java
+++ Java0125/src/part02/evaluation/CLIservice.java
@@ -23,6 +23,8 @@ public class CLIservice {
 			while (!exit) {
 				System.out.println("1. 데이터 삽입");
 				System.out.println("2. 데이터 보기");
+				System.out.println("3. 데이터 수정");
+				System.out.println("4. 데이터 삭제");
 				System.out.println("3. 종료");
 				System.out.print("선택하세요: ");
 
@@ -65,15 +67,42 @@ public class CLIservice {
 		ResultSet rs;
 
 		try {
+			
 			stmt = connection.createStatement();
-			System.out.println("부서번호: ");
-			int deptno = Integer.parseInt(scanner.nextLine());
-			System.out.print("부서이름:");
-			String dname = scanner.nextLine();
-			System.out.print("부서위치:");
-			String loc = scanner.nextLine();
 
-			int result = stmt.executeUpdate("INSERT INTO dept VALUES (" +deptno + ", '"+ dname + "', '" + loc + "')");
+			boolean isThere = true;
+			int empno = -1;
+			while(isThere) {
+				System.out.println("사원번호: ");
+				empno = Integer.parseInt(scanner.nextLine());
+				rs = stmt.executeQuery("SELECT empno FROM emp WHERE empno="+ empno);
+				if(rs.next()) {
+					System.out.println("이미 존재하는 사원번호입니다. 새로운 번호를 입력해주세요.");
+				}else {
+					isThere = false;
+				}
+			}
+			System.out.print("사원이름:");
+			String ename = scanner.nextLine();
+			System.out.print("직무:");
+			String job = scanner.nextLine();
+			System.out.print("고용일자:");
+			String hiredate = scanner.nextLine();
+			System.out.print("임금:");
+			double sal = Double.parseDouble(scanner.nextLine());
+			System.out.print("부서번호:");
+			int deptno = Integer.parseInt(scanner.nextLine());
+
+			int result = -1;
+			if(job.equalsIgnoreCase("salesman")) {
+				System.out.print("커미션:");
+				double comm = Double.parseDouble(scanner.nextLine());
+				result = stmt.executeUpdate("INSERT INTO emp (empno, ename, job, hiredate, sal, comm, deptno) VALUES "
+						+ "('" +empno + "', '"+ ename + "', '" + job + "', '" + hiredate + "', '" + sal + "', '" + comm +"', '" + deptno + "')");
+			}else {
+				result = stmt.executeUpdate("INSERT INTO emp (empno, ename, job, hiredate, sal, deptno) VALUES "
+						+ "('" +empno + "', '"+ ename + "', '" + job + "', '" + hiredate + "', '" + sal + "', '" + deptno + "')");
+			}
 			if(result>=1) {
 				System.out.println("입력성공!");
 			} else {
@@ -130,17 +159,17 @@ public class CLIservice {
 		case "empno":
 		case "mgr":
 		case "deptno":
-			generalQuery(column, stmt, "int");
+			selectQuery(column, stmt, "int");
 			break;
 		case "ename":
 		case "job":
 		case "hiredate":
 		case "*":
-			generalQuery(column, stmt, "string");
+			selectQuery(column, stmt, "string");
 			break;
 		case "sal":
 		case "comm":
-			generalQuery(column, stmt, "double");
+			selectQuery(column, stmt, "double");
 			break;
 		default:
 			System.out.println("검색 조건을 확인해주세요: empno, ename, job, mgr, hiredate, sal, comm, deptno, 또는 *");
@@ -148,7 +177,8 @@ public class CLIservice {
 		}
 	}
 
-	private static void generalQuery(String column, Statement stmt, String type) {
+	
+	private static void selectQuery(String column, Statement stmt, String type) {
 		
 		
 		ResultSet rs = null;
@@ -160,7 +190,7 @@ public class CLIservice {
 				rs = stmt.executeQuery("SELECT * FROM emp ORDER BY empno");
 				printResult(rs);
 			}else if(column.equals("mgr")) {
-				System.out.print("검색할 mgr을 입력해주세요. (NULL일 경우 ENTER 입력)");
+				System.out.print("검색할 mgr을 입력해주세요. (부분검색 가능, NULL일 경우 ENTER 입력)");
 				String input = scanner.nextLine();
 				if(input.isEmpty()) {
 					rs = stmt.executeQuery("SELECT * FROM emp WHERE mgr IS NULL ORDER BY mgr ASC");
@@ -170,17 +200,17 @@ public class CLIservice {
 				}
 				printResult(rs);
 			}else if(column.equals("comm")) {
-				System.out.print("검색할 comm을 입력해주세요. (NULL일 경우 ENTER 입력)");
+				System.out.print("검색할 comm을 입력해주세요. (부분검색 불가, NULL일 경우 ENTER 입력)");
 				String input = scanner.nextLine();
 				if(input.isEmpty()) {
-					rs = stmt.executeQuery("SELECT * FROM emp WHERE mgr IS NULL ORDER BY mgr ASC");
+					rs = stmt.executeQuery("SELECT * FROM emp WHERE comm IS NULL ORDER BY comm ASC");
 				}else {
 					double inputDouble = Double.parseDouble(input);
-					rs = stmt.executeQuery("SELECT * FROM emp WHERE mgr LIKE '%" + inputDouble +"%' ORDER BY mgr ASC");
+					rs = stmt.executeQuery("SELECT * FROM emp WHERE comm =" + inputDouble +" ORDER BY comm ASC");
 				}
 				printResult(rs);			
 			}else {
-				System.out.println("검색할 "+column+"을 입력해주세요.");		
+				System.out.println("검색할 "+column+"을 입력해주세요. (부분검색 가능)");		
 
 				if(type.equalsIgnoreCase("int")) {
 					int input = Integer.parseInt(scanner.nextLine());
@@ -223,7 +253,6 @@ public class CLIservice {
 				}
 			}
 		} catch (SQLException e) {
-			System.out.println("printResult method");
 			e.printStackTrace();	
 		}
 	}//method
@@ -239,46 +268,80 @@ public class CLIservice {
 			System.out.println("검색 조건 입력: empno, ename, job, mgr, hiredate, sal, comm, deptno, 또는 *");
 			String column = scanner.nextLine();
 			setColumn(column, stmt);
-
-
-			/**null data가 있는 column: mgr, comm, **/
-			//			if(column.equalsIgnoreCase("mgr")) {
-			//				System.out.print("검색할 mgr을 입력해주세요. (NULL일 경우 ENTER 입력)");
-			//				String input = scanner.nextLine();
-			//				if(input.isEmpty()) {
-			//					rs = stmt.executeQuery("SELECT * FROM emp WHERE mgr IS NULL ORDER BY mgr ASC");
-			//				}else {
-			//					rs = stmt.executeQuery("SELECT * FROM emp WHERE mgr LIKE '%" + input +"%' ORDER BY mgr ASC");
-			//				}
-			//				printResult(rs);
-			//			}//if
-
-
+			/**null data가 있는 column: mgr, comm **/
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}//catch
 
-
 	}//method R
 
+	
+	private static void updateQuery(String column, Statement stmt, String type, int empno) {
+		
+		ResultSet rs = null;
+		
+		try {
+				System.out.println("수정할 값을 입력해주세요.");
+				if(type.equalsIgnoreCase("int")) {
+					int input = Integer.parseInt(scanner.nextLine());
+					stmt.executeUpdate("UPDATE emp SET " + column + "='"+input+"' WHERE empno="+empno);
+					rs = stmt.executeQuery("SELECT * FROM emp WHERE empno=" + empno);
+					printResult(rs);
+				} else if(type.equalsIgnoreCase("string")){
+					String input = scanner.nextLine();
+					stmt.executeUpdate("UPDATE emp SET " + column + "='"+input+"' WHERE empno="+empno);
+					rs = stmt.executeQuery("SELECT * FROM emp WHERE empno=" + empno);
+					printResult(rs);					
+				} else if(type.equalsIgnoreCase("double")) {
+					double input = Double.parseDouble(scanner.nextLine());
+					stmt.executeUpdate("UPDATE emp SET " + column + "='"+input+"' WHERE empno="+empno);
+					rs = stmt.executeQuery("SELECT * FROM emp WHERE empno=" + empno);
+					printResult(rs);
+				}	
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 	private static void updateData(Connection connection) {
 
 		Statement stmt;
 		ResultSet rs;
+		int empno = 999999999;
 
 		try {
 			stmt = connection.createStatement();
-			stmt.getResultSet();
-			String column = scanner.nextLine();
-			System.out.println("column명: deptno, dname, loc 또는 *");
-			rs = stmt.executeQuery("SELECT * FROM dept");
+			System.out.print("정확한 empno를 입력해주세요: ");
+			empno = Integer.parseInt(scanner.nextLine());
+			rs = stmt.executeQuery("SELECT * FROM emp WHERE empno=" + empno);
+			printResult(rs);
+			//printResult에서 consume한 ResultSet 다시 생성하기
+			rs = stmt.executeQuery("SELECT * FROM emp WHERE empno=" + empno);
+			String salesman = null;
 			while(rs.next()) {
-				System.out.print(rs.getInt("deptno")+ " ");
-				System.out.print(rs.getString("dname")+ " ");
-				System.out.print(rs.getString("loc")+ "\n");
+				if(rs.getString("job").equalsIgnoreCase("salesman")) {
+						salesman = ", comm";
+				}else {
+					salesman = "";
+				}
 			}
-
+			System.out.println("ename, job, mgr, sal" + salesman + ", deptno 중 수정할 항목을 고르세요.");
+			String column = (scanner.nextLine()).toLowerCase();
+			switch(column.toLowerCase()) {
+			case "ename": case "job":
+				updateQuery(column, stmt, "string", empno);
+				break;
+			case "mgr":
+				updateQuery(column, stmt, "int", empno);
+				break;
+			case "sal": case "comm":
+				updateQuery(column, stmt, "double", empno);
+				break;
+			default:
+				break;
+			}
+			 
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -293,15 +356,26 @@ public class CLIservice {
 
 		try {
 			stmt = connection.createStatement();
-			stmt.getResultSet();
-			String column = scanner.nextLine();
-			System.out.println("column명: deptno, dname, loc 또는 *");
-			rs = stmt.executeQuery("SELECT * FROM dept");
-			while(rs.next()) {
-				System.out.print(rs.getInt("deptno")+ " ");
-				System.out.print(rs.getString("dname")+ " ");
-				System.out.print(rs.getString("loc")+ "\n");
+			System.out.print("삭제할 사원번호를 정확히 입력해주세요.");
+			int empno = Integer.parseInt(scanner.nextLine());
+			rs = stmt.executeQuery("SELECT * FROM emp WHERE empno=" +empno);
+			printResult(rs);
+			System.out.println("사원번호 "+empno+"에 대한 정보를 정말로 삭제하시겠습니까? (Y/N) ");
+			String response = scanner.nextLine();
+			
+			if(response.equalsIgnoreCase("y")) {
+				int result = stmt.executeUpdate("DELETE FROM emp WHERE empno="+empno);
+				if(result>=1) {
+					System.out.println("삭제에 성공했습니다.");
+				} else {
+					System.out.println("삭제에 실패했습니다");
+				}
+			}else if(response.equalsIgnoreCase("n")) {
+				System.out.println("삭제를 취소합니다.");
+			}else {
+				System.out.println("부정확한 입력입니다.");
 			}
+			
 
 		} catch (SQLException e) {
 			e.printStackTrace();
