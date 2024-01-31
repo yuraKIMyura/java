0cb57285dee89228bb81c1874262831fe5070051
yuraKIMyura <yurakim.ca23@gmail.com>
2024-01-31 17:27:33 +0900

    Thread

--- Java0131/src/part02/thread/BeepPrint2.java
+++ Java0131/src/part02/thread/BeepPrint2.java
@@ -5,7 +5,7 @@ public class BeepPrint2 {
 	public static void main(String[] args) {
 		
 		/**run()과 start()의 차이 **/
-		//run은 그냥 run method를 실행시킨 것, start는 multi-thread를 시작하는 것
+		//run은 현재 thread에서 method를 실행시킨 것, start는 multi-thread를 시작하는 것
 		Thread thread = new Thread(new BeepTask());
 		thread.start();
 			
